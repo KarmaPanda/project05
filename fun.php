0e7f4cf1825f8f993ae6f31cc7ad737632abb531
AdiraBlumenthal <40038122+AdiraBlumenthal@users.noreply.github.com>
2020-07-02 11:18:59 -0400

    Add files via upload

---
+++ fun.php
@@ -0,0 +1,107 @@
+<?php include "inc/header.php";?>
+<?php include "inc/nav.php";?>
+<?php include "inc/banner.php";?>
+
+	<section class="About">
+		<h2>About the Art</h2>
+		<p>This is a sample of my art portfolio. My work is often conceptual, and my medium of choice is acrylic paint. In 2018-2019, my art concentrated on connections; the way we connect with ourselves and others and the barriers to those connections. Some works from that collection are in this sample and some of these works are other more technical pieces.</p>
+	</section>
+
+		<section>
+			<h2>Art Portfolio</h2>
+			
+			<div class="grid">
+			<div class="box one">
+				<img src="images/metaljar.jpg" alt="metal jar" class="image">
+				<div class="overlay">
+					<div class="text">Metal Jar - Pencil and Acrylic</div>
+				</div>
+			</div>
+
+			<div class="box two">
+				<img src="images/jars.jpg" alt="two jars" class="image">
+				<div class="overlay">
+					<div class="text">Two Jars - Acrylic on Canvas Board</div>
+				</div>
+			</div>
+
+			<div class="box three">
+				<img src="images/threehands.jpg" alt="three hands clasping" class="image">
+				<div class="overlay">
+					<div class="text">Hands Touching Hands - Acrylic on Canvas Board</div>
+				</div>
+			</div>
+
+			<div class="box four">
+				<img src="images/equality.jpg" alt="sign for equality" class="image">
+				<div class="overlay">
+					<div class="text">Equality - Pencil</div>
+				</div>
+			</div>
+
+			<div class="box five">
+				<img src="images/bootandanchor.jpg" alt="boot and anchor" class="image">
+				<div class="overlay">
+					<div class="text">Boot and Anchor</div>
+				</div>
+			</div>
+
+			<div class="box six">
+				<img src="images/self.jpg" alt="self portrait in reds" class="image">
+				<div class="overlay">
+					<div class="text">Self Portrait in Reds - Acrylic on Canvas</div>
+				</div>
+			</div>
+			
+			
+			<div class="box seven">
+				<img src="images/covered.jpg" alt="woman covering eyes" class="image">
+				<div class="overlay">
+					<div class="text">Covered Eyes - Acrylic on Canvas</div>
+				</div>
+			</div>
+			
+			<div class="box eight">
+				<img src="images/eyes.jpg" alt="eye contact" class="image">
+				<div class="overlay">
+					<div class="text">Eye Contact - Acrylic on Canvas</div>
+				</div>
+			</div>
+
+			<div class="box nine">
+				<img src="images/folk.jpg" alt="painting of two faces and the quotes 'nothing to fear but fear itself' and 'slow and steady wins the race'" class="image">
+				<div class="overlay">
+					<div class="text">Folktales and Phobias - Mixed Media</div>
+				</div>
+			</div>
+
+			<div class="box ten">
+				<img src="images/branches.jpg" alt="branches" class="image">
+				<div class="overlay">
+					<div class="text">Branches - Scrathboard</div>
+				</div>
+			</div>
+			
+			<div class="box eleven">
+				<img src="images/colors.jpg" alt="abstract painting" class="image">
+				<div class="overlay">
+					<div class="text">In the Air - Acrylic on Canvas</div>
+				</div>
+			</div>
+			
+			<div class="box twelve">
+				<img src="images/connect.jpg" alt="shows how we connect through different identities" class="image">
+				<div class="overlay">
+					<div class="text">Connections - Mixed Media</div>
+				</div>
+			</div>
+			</div>
+			
+
+
+		</section>
+<?php include "inc/scripts.php";?>
+
+
+</body>
+</html>
