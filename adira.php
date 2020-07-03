1fd126c578d90f29037ba3600c916f33f03a1aea
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-07-03 12:01:49 -0400

    Mass Change
    +Structure and Content added
    +Basic nav added
    +Changed file names to .php
    +Added inc and js folder
    - Took out forms from Alejandro's file

---
+++ adira.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adira Blumenthal</title>
+</head>
+
+<body>
+    <header>
+        <h1>Adira Blumenthal</h1></a>
+        <ul>
+            <li>University of Rochester, BS in Computer Science, 2023</li>
+            <li>Lighting Designer and Stage Manager</li>
+        </ul>
+    </header>
+
+    <section>
+        <h2>Overview</h2>
+        <p>I'm a rising sophomore at University of Rochester majoring in Computer Science and minoring
+            in Brain and Cognitive Science. I'm from Silver Spring, Maryland, which is right outside of
+            Washington, DC. I was a dancer for eight years, and my senior year I switched over from being
+            on stage to behind the scenes, doing lighting, sound, and stage management. </p>
+    </section>
+
+    <section>
+        <header>
+            <h2>Interests</h2>
+            <p>I'm interested in using technology to help people with disabilities be more independent.
+                Assistive technology has the power to enable independence, foster learning, facilitate
+                opportunities, and empower people with disabilities. I am studying Computer Science and
+                Brain and Cognitive Science in order to merge my interest for technology and my passion
+                for helping people with disabilities.</p>
+        </header>
+
+        <h2>Education</h2>
+        <div>University of Rochester, Rochester, NY</div>
+        <ul>
+            <li>Expected graduation: May 2023</li>
+            <li>Major: Computer Science</li>
+            <li>Minor: Brain and Cognitive Science</li>
+            <li>Clusters: Theater Production & Psychology</li>
+        </ul>
+
+        <h2>Awards and Skills</h2>
+        <h3>Coding Languages</h3>
+        <ul>
+            <li>Proficient in Java, C, HTML, CSS</li>
+            <li>Familiar with JavaScript and PHP in web development</li>
+            <li>Some experience with Python</li>
+        </ul>
+
+        <div>Honorable Mention, Writing Colloquium Contest, 2020</div>
+
+        <h3>Research Projects</h3>
+        <ul>
+            <li>Online Behavior Project - Comparing online behavior during COVID-19 to online
+                behavior pre-COVID-19 and examining the relationship between online behavior
+                and mental health </li>
+            <li>ASL Project - building a device to help hearing parents of deaf children learn
+                sign language and teach it to their child</li>
+        </ul>
+    </section>
+
+    <section>
+        <div>Experiences</div>
+
+        <h2>University of Rochester</h2>
+
+        <h3>Production Team, ROC Players</h3>
+        <ul>
+            <li>Assistant lighting designer - Fall 2019</li>
+            <li>Stage Manager - Spring 2020</li>
+            <li>Lighting Designer - Fall 2020</li>
+        </ul>
+
+        <h3>Member, Society of Women Engineers</h3>
+        <p>Helped plan the “Girls Workshop” where we teach girls in elementary school about various
+            engineering fields; ran the computer science activity. </p>
+
+        <aside class="summer">
+            <h2>Summers</h2>
+            <h3>Nachshon Project Fellow, summer 2020</h3>
+            <p>Participated in a three-week leadership training including sessions on Jewish education,
+                working with children, and programming skills</p>
+
+            <h3>Bunk Counselor, Camp Ramah New England, Palmer, MA, summer 2019</h3>
+            <ul>
+                <li>Programmed evening activities for rising eighth graders</li>
+                <li>Started a club for LGBTQ teen campers to make camp more LGBTQ-inclusive;
+                    club published a magazine on LGBTQ issues</li>
+            </ul>
+        </aside>
+    </section>
+
+    <section>
+        <header>
+            <h2>About the Art</h2>
+            <p>This is a sample of my art portfolio. My work is often conceptual, and my medium of choice
+                is acrylic paint. In 2018-2019, my art concentrated on connections; the way we connect with 
+                ourselves and others and the barriers to those connections. Some works from that collection
+                are in this sample and some of these works are other more technical pieces.</p>
+        </header>
+
+        <h2>Art Portfolio</h2>
+
+        <div class="grid">
+            <div class="box one">
+                <img src="images/metaljar.jpg" alt="metal jar" class="image">
+                <div class="overlay">
+                    <div class="text">Metal Jar - Pencil and Acrylic</div>
+                </div>
+            </div>
+
+            <div class="box two">
+                <img src="images/jars.jpg" alt="two jars" class="image">
+                <div class="overlay">
+                    <div class="text">Two Jars - Acrylic on Canvas Board</div>
+                </div>
+            </div>
+
+            <div class="box three">
+                <img src="images/threehands.jpg" alt="three hands clasping" class="image">
+                <div class="overlay">
+                    <div class="text">Hands Touching Hands - Acrylic on Canvas Board</div>
+                </div>
+            </div>
+
+            <div class="box four">
+                <img src="images/equality.jpg" alt="sign for equality" class="image">
+                <div class="overlay">
+                    <div class="text">Equality - Pencil</div>
+                </div>
+            </div>
+
+            <div class="box five">
+                <img src="images/bootandanchor.jpg" alt="boot and anchor" class="image">
+                <div class="overlay">
+                    <div class="text">Boot and Anchor</div>
+                </div>
+            </div>
+
+            <div class="box six">
+                <img src="images/self.jpg" alt="self portrait in reds" class="image">
+                <div class="overlay">
+                    <div class="text">Self Portrait in Reds - Acrylic on Canvas</div>
+                </div>
+            </div>
+
+            <div class="box seven">
+                <img src="images/covered.jpg" alt="woman covering eyes" class="image">
+                <div class="overlay">
+                    <div class="text">Covered Eyes - Acrylic on Canvas</div>
+                </div>
+            </div>
+
+            <div class="box eight">
+                <img src="images/eyes.jpg" alt="eye contact" class="image">
+                <div class="overlay">
+                    <div class="text">Eye Contact - Acrylic on Canvas</div>
+                </div>
+            </div>
+
+            <div class="box nine">
+                <img src="images/folk.jpg"
+                    alt="painting of two faces and the quotes 'nothing to fear but fear itself' and 'slow and steady wins the race'"
+                    class="image">
+                <div class="overlay">
+                    <div class="text">Folktales and Phobias - Mixed Media</div>
+                </div>
+            </div>
+
+            <div class="box ten">
+                <img src="images/branches.jpg" alt="branches" class="image">
+                <div class="overlay">
+                    <div class="text">Branches - Scrathboard</div>
+                </div>
+            </div>
+
+            <div class="box eleven">
+                <img src="images/colors.jpg" alt="abstract painting" class="image">
+                <div class="overlay">
+                    <div class="text">In the Air - Acrylic on Canvas</div>
+                </div>
+            </div>
+
+            <div class="box twelve">
+                <img src="images/connect.jpg" alt="shows how we connect through different identities" class="image">
+                <div class="overlay">
+                    <div class="text">Connections - Mixed Media</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+<?php include "inc/scripts.php";?>
+
+</html>
---
+++ inc/nav.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+	<ul>
+		<li><a href="index.php"></a></li>
+		<li> <a href="adira.php">Adira</a></li>
+		<li> <a href="alejandro.php">Alejandro</a></li>
+		<li> <a href="leah.php">Leah</a></li>
+	</ul>
+</nav>
