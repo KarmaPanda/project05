eec7294e3f6d11aa4260359b87ca5a4ffc91ffea
Adira Blumenthal <adiraeb@gmail.com>
2020-07-10 14:31:04 -0400

    initial file organization

--- inc/nav.php
+++ inc/nav.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="index.php">Home</a></li>
 		<li> <a href="adira.php">Adira</a></li>
-		<li> <a href="alejandro.php">Alejandro</a></li>
-		<li> <a href="leah.php">Leah</a></li>
+		<li> <a href="jason.php">Jason</a></li>
+		<li> <a href="yilin.php">Yilin</a></li>
 	</ul>
 </nav>
