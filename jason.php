e343c5953094a4f32b9265ca77c299f34aa8e117
jkat718 <jkat718@users.noreply.github.com>
2020-07-11 12:21:48 -0400

    Update jason.php
    Transferred content from Project 1.

--- jason.php
+++ jason.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alejandro Ramirez | Team LA</title>
+    <title>Jason Katzner | Team LA</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
@@ -15,88 +15,132 @@
     <?php include "inc/nav.php"; ?>
 
     <header>
-        <a href="index.php" target="_self">
-            <h1>Alejandro Ramirez</h1>
+        <a href="index.php" target="_self" class="title_link">
+            <h1>Jason Katzner</h1>
         </a>
         <div class="top">
-        <img src="images/met_resized3.jpg" alt="alejandro">
+        <img src="images/jason.jpg" alt="Jason">
         <ul>
-            <li>University of Rochester, BA in Computer Science</li>
-            <li>Class of 2022</li>
-        </ul></div>
+            <li>University of Rochester, BA in Computer Science, 2021</li>
+            <li>LARPer and Future Educator</li>
+        </ul>
+        </div>
     </header>
 
     <section>
         <h2>Overview</h2>
-        <p>I'm a rising junior from Long Island, New York. I'm majoring in Computer Science
-            and minoring in Digital Media Studies. Before UR, I spent the first semester of
-            my college career at RPI studying Mechanical Engineering. Aside from computing,
-            I love cooking, volunteering, hiking/the outdoors, and music!</p>
+        <p>Jason Katzner (he/him) is a member of the University of Rochester class of 2021 majoring in
+		    Computer Science. He is also in the GRADE program, studying Online Teaching & Learning. He also
+		    studies Linguistics and Technical Theater. Outside of his studies, he lives on the Computer
+		    Interest Floor, plays tabletop roleplaying games, and LARPs. </p>
     </section>
 
-    <section>
-        <header>
-            <h2>Interests</h2>
+        <article>
+            <h2>Computer Science</h2>
 
-            <p>I'm interested in computer vision, security, and machine learning - and how we
-                can use these technologies to help people in their everyday life and their jobs.</p>
-        </header>
-        <div class="edu">
-            <div class="i">
-                <h2>Education</h2>
+            <section>
+                <h3>Bachelor's</h3>
 
-                <p>University of Rochester, Rochester NY</p>
 
-                <ul>
-                    <li>Major: Computer Science</li>
-                    <li>Minor: Digital Media Studies (Humanities)</li>
-                    <li>Cluster: Psychology as a Social Science</li>
+                <p>Jason is pursuing a Bachelor of Arts in Computer Science from the Hajim School. Originally on a focus track in Natural Language Understanding, he is considering a last-minute switch to Human-Computer Interaction (HCI). On the professional side of computer science, Jason has worked as an intern at Accrete, a financial technology startup specializing in artificial-intelligence generated market analysis.</p>
+
+                <aside>
+                    <h4>Coursework</h4>
+
+                    <ul>
+                        <li>CSC	170: Intro to Web Design & Development</li>
+                        <li>CSC	172: Data Structures & Algorithms</li>
+                        <li>CSC	173: Computation and Formal Systems</li>
+                        <li>CSC	242: Artificial Intelligence</li>
+                        <li>CSC	250: Data Science for Linguistics</li>
+                        <li>CSC	252: Computer Organization</li>
+                        <li>CSC	261: Intro to Databases</li>
+                        <li>CSC	280: Computer Models & Limitations</li>
+                        <li>CSC	273W: Writing for Computer Science</li>
+                        <li>CSC	275: Creative Computing</li>
+                        <li>MTH 150: Discrete Mathematics</li>
+                        <li>WRT 105: Posthumanism</li>
+                        <li>WRT 273: Engineering - Communicating Your Professional Identity</li>
+                    </ul>
+                </aside>
+            </section>
+        </article>
+
+        <article>
+            <h2>Education</h2>
+
+            <section>
+                <h3>Master's</h3>
+
+                <p>Jason is pursuing a Master's of Education from the Warner School via the Guaranteed Rochester Accelerated Degree in Education (GRADE) program. He is intending to utilize the combined degree option to also complete an Advanced Certification in Digitally-Rich Teaching in K-12 Schools. Outside of formal education, Jason has worked in afterschool and summer programs, as well as tutoring students in the Java programming language.</p>
+
+                <aside>
+                    <h4>Coursework</h4>
+
+                    <ul class="course-list">
+                        <li>EDE	484: Online Teaching & Learning</li>
+                        <li>EDE 486: Designing Online Courses</li>
+                    </ul>
+                </aside>
+            </section>
+        </article>
+
+        <section>
+            <h3>Minor: Linguistics</h3>
+            <p>Jason has completed a cluster in Language Structure via the linguistics department. This cluster is defined as one that "focuses on the semantics and pragmatics of natural language and how linguistic forms and their usages give rise to meanings" (<a href="https://secure1.rochester.edu/registrar/CSE/clusterView.php?clusterNumber=S1LIN005" target="_blank">source</a>). In the fall, Jason plans to finish all remaining coursework to turn this cluster into a minor in linguistics.</p>
+
+            <aside>
+                <h4>Coursework</h4>
+                <ul class="course-list">
+                    <li>LIN	110: Introduction to Linguistic Analysis</li>
+                    <li>LIN	210: Introduction to Language Sound Systems</li>
+                    <li>LIN	220: Introduction to Grammatical Systems</li>
+                    <li>LIN	225: Introduction to Semantic Analysis (minor, planned)</li>
+                    <li>LIN	250: Data Science for Linguistics (minor)</li>
                 </ul>
+            </aside>
+        </section>
 
-                <p>For the Fall 2020 semester, I will be a TA for CSC 214</p>
-            </div>
-            <div class="awards">
-                <h3>CS classes I've taken:</h3>
+        <section>
+            <h3>Cluster: Theatre</h3>
+            <p>Jason has also completed a cluster in Theatre Production and Performance via the theater department (housed in the English department), and then some. He originally intended on completing a certificate in stage management, but realized the extracurricular requirements were too steep to complete alongside a master's degree.</p>
 
-                <ul>
-                    <li>CSC 170: Intro to Web Development</li>
-                    <li>CSC 171: Intro to Computing Science</li>
-                    <li>CSC 172: Data Structures and Algorithms</li>
-                    <li>CSC 173: Computation and Formal Systems</li>
-                    <li>CSC 214: Mobile Application Development (iOS - Swift)</li>
-                    <li>CSC 299W: Social Implications of Computing</li>
+            <aside>
+                <h4>Coursework</h4>
+                <ul class="course-list">
+                    <li>ENG	154: Intro to Design for Stage (cluster)</li>
+                    <li>ENG	173: Intro to Stage Lighting and Sound (cluster)</li>
+                    <li>ENG	178: Design for Stage: Costumes</li>
+                    <li>ENG	178: Design for Stage: Sound (cluster)</li>
+                    <li>ENG	179: Design for Stage: Lighting</li>
+                    <li>ENG	179: Design for Stage: Scenic</li>
+                    <li>ENG	271: Advanced Technical Theater (cluster)</li>
+                    <li>ENG	297: Stage Management: Spring 2019 (cluster)</li>
+                    <li>ENG	299: Performance Lab: Conduct of Life</li>
                 </ul>
-            </div>
-        </div>
-    </section>
+            </aside>
+        </section>
 
-    <section>
+        <section>
+            <h3>Electives</h3>
+            <p>Jason occasionally takes some classes that don't count toward his major, grad program, minor, or cluster. These courses are just for fun...and to meet the minimum required 128 credit hours, of course!</p>
 
-        <h2>Experiences</h2>
-
-        <h3>Engineers without Borders, University of Rochester Chapter</h3>
-
-        <h4>Project Lead - Rochester, NY</h4>
-
-        <p class="desc">My team and I are working on providing clean drinking water to a
-            rural Tanzanian village. Currently, they have to travel 20km each way to a
-            contaminated water source. I have several duties like coordinating meetings,
-            communicating with contacts throughout the world, and writing applications
-            for the national organization.</p>
-
-        <h3>Patchogue Neighbors Soup Kitchen</h3>
-
-        <h4>Line Cook, Volunteer Coordinator - Patchogue, NY</h4>
-
-        <p class="desc">I started volunteering here in 2012. I cook breakfast for 100+
-            people, and I coordinate volunteers from the community and local school district. </p>
-    </section>
-
-    <a href="index.php" target="_self" class="link end">Back to Home Page</a>
-
-
+            <aside>
+                <h4>Coursework</h4>
+                <ul class="course-list">
+                    <li>EAS	141: Basic Mechanical Fabrication</li>
+                    <li>EAS	143: Introduction to Solid Modeling</li>
+                    <li>MUR	125: History of Rock Music</li>
+                </ul>
+            </aside>
+        </section>
+    </main>
+    <footer>
+        Taken from <a href="http://csc174.org/lab01/jkatzner/lab01" target="_blank">csc174.org/lab01/jkatzner/lab01</a>
+        <p>CSC 174: Advanced Front-end Web Design and Development</p>
+		<a href="index.php" target="_self" class="link end">Back to Home Page</a>
+    </footer>
     <?php include "inc/scripts.php"; ?>
-
 </body>
 
 </html>
