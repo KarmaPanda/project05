f563eab7045c2cd6f661add6f884dfda6abdc5bf
Adira Blumenthal <adiraeb@gmail.com>
2020-07-12 12:42:44 -0400

    Updated title tags

--- jason.php
+++ jason.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jason Katzner | Team LA</title>
+    <title>Jason Katzner | Team Chicago</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
--- yilin.php
+++ yilin.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leah Goodwin | Team LA</title>
+    <title>Leah Goodwin | Team Chicago</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
