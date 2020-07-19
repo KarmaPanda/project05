6ca67c20f4a8f46780c86d0e91e108133131c7b5
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-07-19 11:31:46 -0400

    Beginning of a basic placeholder for the login page

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php include "inc/html_top.php"; ?>
+
+<body>
+	<nav class="menu">
+         <a href="index.php">
+            <figure id="padlogo">
+                <img src="images/logo_small.png" alt="Class Logo">
+            </figure>
+        </a>
+	</nav>
+
+    <header>
+       <h1>Login here</h1>
+	   <div>Username: _______</div>
+	   <div>Password: _______</div>
+	   <div>Go!</div>
+    </header>
+</body>
+
+</html>
