c48dd76020f6fea634b6c5cdb33ec0107e3f48d9
Adira Blumenthal <adiraeb@gmail.com>
2020-07-29 17:42:32 -0400

    edit studentlist.php

--- studentlist.php
+++ studentlist.php
@@ -1,3 +1,17 @@
+<?php
+    // 1. Create a database connection
+    $dbhost = "66.147.242.186";
+    $dbuser = "urcscon3_chicago";
+    $dbpass = "project4chicago";
+    $dbname = "urcscon3_chicago";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+    // 2. Perform database query
+    $query  = "SELECT * FROM classinfo";
+    $result = mysqli_query($connection, $query);
+?>
+
 <?php include "inc/html_top.php"; ?>
 
 <body class="container fgrid">
@@ -17,53 +31,29 @@
         <p>Team Chicago is a trio comprised of University
             of Rochester students all majoring in Computer Science. Although all three major
             in Computer Science, you'll find they each have their own charms and interests.
-            Read their profiles below to find out more about them.
+            Read their profiles below to find out more about them.</p>
     </header>
 
     <div id = "f-page-div" class = "f-page">
-
-        <figure>
-            <img src="images/senior.jpg" alt="Adira Photo">
-        </figure>
-
-
-        <article>
-            <h2>Adira Blumenthal</h2>
-                <p> Adira Blumenthal (she/her) is from Silver Spring, Maryland and is a member of the University of Rochester class of 2023 majoring in Computer Science. She was a dancer for many years but in 2018, she switched over from being on stage to behind the scenes, doing lighting, sound, and stage management for her dance studio. Now, she continues her interest in technical theater at the University of Rochester by her involvement in Roc Players and Todd Productions. </p>
-        </article>
-
-        <!-- <figure class="rel"> -->
-                <a href="adira.php" class="f-page-link-adria"><img src="images/stars_resized.png" alt="view page stars"><div>View Adira's Page!</div></a>
-        <!-- </figure> -->
+        <?php
+            // 3. Use returned data (if any)
+            while($data = mysqli_fetch_assoc($result)) {
+        ?>
+            <figure>
+                <?php echo "<img src=\"$photo\"/>";?>
+            </figure>
 
 
-        <figure>
-            <img src="images/jason.jpg" alt="Jason Photo">
-        </figure>
+            <article>
+                <h2><?php echo $data["firstName"], " ", $data["lastName"];?></h2>
+                    <p><?php echo $data["info"];?></p>
+            </article>
 
-        <article>
-            <h2>Jason Katzner</h2>
-                <p>Jason Katzner (he/him) is a member of the University of Rochester class of 2021 majoring in Computer Science. He is also in the GRADE program, studying Online Teaching & Learning. He also studies Linguistics and Technical Theater. Outside of his studies, he lives on the Computer Interest Floor, plays tabletop roleplaying games, and LARPs.</p>
-        </article>
+                    <a href="<?php echo $link;?>" class="f-page-link-adria"><img src="images/stars_resized.png" alt="view page stars"><div>Read More!</div></a>
+        }
+</div>
 
-        <!-- <figure class="rel">  -->
-                <a href="jason.php" class="f-page-link-jason"><img src="images/stars_resized.png" alt="view page stars"><div>View Jason's Page!</div></a>
-        <!-- </figure> -->
 
-        <figure>
-                <img src="images/yilinluo.jpg" alt="Jason Photo">
-        </figure>
-
-        <article>
-            <h2>Yilin Luo</h2>
-                <p>Yilin Luo is a member of the University of Rochester, class of 2021. She double majored in Computer Science and Studio Arts. Her focus is Human Computer Interaction. With the passion to improve technology UX, she interned at Open Letter Books in summer 2021 as a Website Developer and Marketing Intern. Yilin is keen on bringing enjoyment in arts, she has also volunteered as an art workshop leader at Huther Doyle.</p>
-        </article>
-
-    </div>    
-
-    <!-- <figure class="rel"> -->
-            <a href="yilin.php" class="f-page-link-yilin"><img src="images/stars_resized.png" alt="view page stars"><div>View Yilin's Page!</div></a>
-    <!-- </figure> -->
 
 
     <?php include "inc/scripts.php"; ?>
