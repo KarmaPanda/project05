b2b28f980576c9a3a94c88a637d45b8e18392311
Adira Blumenthal <adiraeb@gmail.com>
2020-07-29 16:58:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,40 @@
             Read their profiles below to find out more about them.
     </header>
 
+
+    <?php
+    // 1. Create a database connection
+    $dbhost = "66.147.242.186";
+    $dbuser = "urcscon3_chicago";
+    $dbpass = "project4chicago";
+    $dbname = "urcscon3_chicago";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+    // 2. Perform database query
+    $query  = "SELECT * FROM classinfo";
+    $result = mysqli_query($connection, $query);
+?>
+
     <article>
+        <ul>
+        <?php
+            // 3. Use returned data (if any)
+            while($data = mysqli_fetch_assoc($result)) {
+        ?>
+        <li>
+            <figure><img src=<?php echo $data["photo"];?>></figure>
+            <h2><?php echo 
+                $data["firstName"], " ", $data["lastName"]; 
+            ?></h2>
+            <p><?php echo $data["info"];?></p>
+            <a href=<?php echo $data["link"];?>>Read More!</a>
+        </li>
+        <?php } ?>
+    </ul>
+    </article>
+
+    <!-- <article>
         <h2>Adira Blumenthal</h2>
         <p> Adira Blumenthal (she/her) is from Silver Spring, Maryland and is a member of the University of Rochester class of 2023 majoring in Computer Science. She was a dancer for many years but in 2018, she switched over from being on stage to behind the scenes, doing lighting, sound, and stage management for her dance studio. Now, she continues her interest in technical theater at the University of Rochester by her involvement in Roc Players and Todd Productions. </p>
         <a href="adira.php" target="_self" class="link"> Read more about Adira</a>
@@ -38,7 +71,7 @@
         <h2>Yilin Luo</h2>
         <p>Yilin Luo is a member of the University of Rochester, class of 2021. She double majored in Computer Science and Studio Arts. Her focus is Human Computer Interaction. With the passion to improve technology UX, she interned at Open Letter Books in summer 2021 as a Website Developer and Marketing Intern. Yilin is keen on bringing enjoyment in arts, she has also volunteered as an art workshop leader at Huther Doyle.</p>
         <a href="yilin.php" target="_self" class="link"> Read more about Yilin</a>
-    </article>
+    </article> -->
 
     <?php include "inc/scripts.php"; ?>
 
