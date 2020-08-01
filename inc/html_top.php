c5c1f3e0c26ed62e37678f3d25ea529c7f4548ea
Joo Eon Park <jpark174@u.rochester.edu>
2020-08-01 22:36:13 +0900

    Student list edits

--- inc/html_top.php
+++ inc/html_top.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 3: Team Chicago</title>
+    <title>Project 4: Team Chicago</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Mono&display=swap" rel="stylesheet">
