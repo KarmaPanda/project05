c66da6931d8aa9268fa57c515d759351c72b6f23
Joo Eon Park <jpark174@u.rochester.edu>
2020-08-01 23:28:15 +0900

    Styling to db.php

--- db.php
+++ db.php
@@ -2,9 +2,22 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Class Info</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- Override CSS -->
+  <link rel="stylesheet" type="text/css" href="css/override.css">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="css/nav.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Mono&display=swap" rel="stylesheet">
+
+	<title>Your Account</title>
 </head>
-<body>
+<body class="container dbgrid">
 <?php
 // connect to the database
 include('connect-db.php');
@@ -12,6 +25,17 @@ include('connect-db.php');
 // get results from database
 $result = mysqli_query($connection, "SELECT * FROM classinfo");
 ?>
+
+  <nav class="menu">
+         <a href="index.php">
+            <figure id="padlogo">
+                <img src="images/logo_small.png" alt="Class Logo">
+            </figure>
+        </a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+    </ul>
+  </nav>
 
 <table border>
   <tr>
@@ -42,9 +66,14 @@ while($row = mysqli_fetch_array( $result )) {
 </table>
 
 <div>
-  <br>
-	<a href="new.php">Add a new record</a>
+  <a class="btn btn-info" href="new.php">Add a new record</a>
 </div>
+
+<!-- Javascript for Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </body>
 </html>
 <?php
