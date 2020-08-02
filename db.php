33fa693c0c88cc79f934319fc0ad19958a70bcfb
Joo Eon Park <jpark174@u.rochester.edu>
2020-08-02 14:37:47 +0900

    table style

--- db.php
+++ db.php
@@ -40,28 +40,32 @@ $result = mysqli_query($connection, "SELECT * FROM classinfo");
     </ul>
   </nav>
 
-<table border>
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Blurb</th>
-    <th>Quote</th>
-    <th>Link</th>
-    <th colspan="2"><em>functions</em></th>
-  </tr>
+<table class="table table-bordered table-secondary">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Blurb</th>
+      <th scope="col">Quote</th>
+      <th scope="col">Link</th>
+      <th scope="col" colspan="2"><em>functions</em></th>
+    </tr>
+  </thead>
 <?php
 // loop through results of database query, displaying them in the table
 while($row = mysqli_fetch_array( $result )) {
 ?>
-  <tr>
-    <td><?php echo $row['firstname']; ?></td>
-    <td><?php echo $row['lastname']; ?></td>
-    <td><?php echo $row['info']; ?></td>
-    <td><?php echo $row['quote']; ?></td>
-    <td><?php echo $row['link']; ?></td>
-    <td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
-    <td><a onclick="return confirm('Are you sure you want to delete: <?php echo $row["firstname"] . " " . $row["lastname"]; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
-  </tr>
+  <tbody>
+    <tr>
+      <td scope="row"><?php echo $row['firstname']; ?></td>
+      <td><?php echo $row['lastname']; ?></td>
+      <td><?php echo $row['info']; ?></td>
+      <td><?php echo $row['quote']; ?></td>
+      <td><?php echo $row['link']; ?></td>
+      <td><a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+      <td><a onclick="return confirm('Are you sure you want to delete: <?php echo $row["firstname"] . " " . $row["lastname"]; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+    </tr>
+  </tbody>
 <?php
 // close the loop
 }
