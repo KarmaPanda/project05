612d3af30ce990fa89397585c9262b5a5cab670a
efreling <67718969+efreling@users.noreply.github.com>
2020-08-01 22:28:10 -0400

    Add header

--- portal.php
+++ portal.php
@@ -1,5 +1,16 @@
 <?php include "inc/html_top.php"; ?>
 
+<header class="persistent">
+	<div class="containerz">
+		<div class="primary">
+			<!-- Primary Optical Area -->
+			<a href="index.php"><img src="images/logo.png" alt="Logo"></a>
+		</div>
+		<div class="middle">CSC 174 - Advanced Front End Web Design and Development</div>
+		</div>
+	</div><!-- .containerz -->
+</header>
+
 <body class="container_portal">
 
 	<a href="new.php" class="option">Add New Profile</a>
