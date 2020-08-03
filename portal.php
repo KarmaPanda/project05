7ea815ed972928d472d1499c38bf6729d71d77ce
Adira Blumenthal <adiraeb@gmail.com>
2020-08-03 16:45:54 -0400

    Update portal.php

--- portal.php
+++ portal.php
@@ -7,16 +7,15 @@
 			<a href="index.php"><img src="images/logo_small.png" alt="Logo" id="padlogo"></a>
 		</div>
 		<h1 class="middle">CSC 174 - Advanced Front End Web Design and Development</h1>
-		</div>
-	</div><!-- .containerz -->
+	</div>
 </header>
 
-<body class="container_portal">
+<section class="container_portal">
 
 	<a href="new.php" class="option">Add New Profile</a>
 
 	<a href="db.php" class="option">Edit Profile</a>
 
 	<a href="db.php" class="option">Delete Profile</a>
-</body>
+</section>
 </html>
