88937c4a8e02720a7779e350a3155e6c9e4f508d
efreling <67718969+efreling@users.noreply.github.com>
2020-08-03 08:23:38 -0400

    Organize renderform.css, move popup.php out of the inc folder and adjust links to correspond

---
+++ popup.php
@@ -0,0 +1,12 @@
+<?php include "inc/html_top.php"; ?>
+
+	<body class="popup_container">
+
+		<h1>Record Added!</h1>
+		<p>Go check out your new, fancy profile.</p>
+
+		<a href="db.php" class="viewall meet">Meet your classmates!</a>
+
+		<a href="index.php" class="viewall">Home</a>
+	</body>
+</html>
