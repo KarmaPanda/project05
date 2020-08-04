7c1ca0a312a89137157f8699e684faa8c5d00f3b
efreling <67718969+efreling@users.noreply.github.com>
2020-08-04 08:49:55 -0400

    fix some errors

--- portal.php
+++ portal.php
@@ -6,18 +6,18 @@
 			<!-- Primary Optical Area -->
 			<a href="index.php"><img src="images/logo_small.png" alt="Logo" id="padlogo"></a>
 		</div>
+			<h1 class="middle">CSC 174 - Advanced Front End Web Design and Development</h1>
+
 	</div>
 </header>
 
 
-<section class="container_portal">
-	<h1 class="middle">CSC 174 - Advanced Front End Web Design and Development</h1>
-
+<div class="container_portal">
 	<a href="new.php" class="option">Add New Profile</a>
 
 	<a href="db.php" class="option">Edit Profile</a>
 
 	<a href="db.php" class="option">Delete Profile</a>
-</section>
+</div>
 </body>
 </html>
